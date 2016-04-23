bba90202bd01547c96769801ff3b7f003e91fd7b
Jean Tessier <jean@directly.com>
2016-04-23 09:57:01 -0700

    Use express-jwt module to secure DELETE route.

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('express-jwt');
+var auth = jwt({
+    secret: process.env.JWT_SECRET,
+    userProperty: 'payload'
+});
+
 var ctrlBooks = require('../controllers/books');
 var ctrlAuth = require('../controllers/authentication');
 
@@ -7,7 +13,7 @@ router.get('/books', ctrlBooks.booksList);
 // router.post('/books', ctrlBooks.booksCreate);
 router.get('/books/:bookid', ctrlBooks.booksReadOne);
 // router.put('/books/:bookid', ctrlBooks.booksUpdateOne);
-router.delete('/books/:bookid', ctrlBooks.booksDeleteOne);
+router.delete('/books/:bookid', auth, ctrlBooks.booksDeleteOne);
 
 router.post('/register', ctrlAuth.register);
 router.post('/login', ctrlAuth.login);
