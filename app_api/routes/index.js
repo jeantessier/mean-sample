afc80933723363942f59efd514e099046fae9243
Jean Tessier <jean@directly.com>
2016-04-23 09:45:34 -0700

    Use password module to manage registration and logging in.

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var ctrlBooks = require('../controllers/books');
+var ctrlAuth = require('../controllers/authentication');
 
 router.get('/books', ctrlBooks.booksList);
 // router.post('/books', ctrlBooks.booksCreate);
 router.get('/books/:bookid', ctrlBooks.booksReadOne);
 // router.put('/books/:bookid', ctrlBooks.booksUpdateOne);
 router.delete('/books/:bookid', ctrlBooks.booksDeleteOne);
+
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
 
 module.exports = router;
