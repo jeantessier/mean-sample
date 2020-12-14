d1a9afde2cb91687c53f3ec0780008087eace02d
Jean Tessier <jean@jeantessier.com>
2020-12-13 23:11:40 -0800

    Upgrade Mongoose and Dotenv
    Obtained the target versions with `npm outdated`.  The new versions
    required some manual changes in the code.
    
    Removed the default database URI.  And changed the name of the
    environment parameter from `MONGOLAB_URI` to `MONGODB_URI`, since
    MongoLab does not exist anymore.  You now HAVE to specify `MONGODB_URI`
    in the environment, through an environment variable or an entry in the
    `.env` file.
    
    In `db.js`, I brought the style closer to ES6, like using arrow
    functions and moving away from `var` variables.

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,46 +1,43 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var dbURI = "mongodb://localhost/jeantessier";
-if (process.env.NODE_ENV === 'production') {
-    dbURI = process.env.MONGOLAB_URI;
-}
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true});
+const dbURI = process.env.MONGODB_URI;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
-mongoose.connection.on("connected", function() {
+mongoose.connection.on("connected", () => {
     console.log("Mongoose connected to " + dbURI);
 });
 
-mongoose.connection.on("error", function(err) {
+mongoose.connection.on("error", err => {
     console.log("Mongoose connection error: " + err);
 });
 
-mongoose.connection.on("disconnected", function() {
+mongoose.connection.on("disconnected", () => {
     console.log("Mongoose disconnected");
 });
 
 // CAPTURE APP TERMINATION / RESTART EVENTS
 // To be called when process is restarted or terminated
-var gracefulShutdown = function(msg, callback) {
-    mongoose.connection.close(function() {
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close(() => {
         console.log('Mongoose disconnected through ' + msg);
         callback();
     });
 };
 // For nodemon restarts
-process.once('SIGUSR2', function() {
-    gracefulShutdown('nodemon restart', function() {
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
         process.kill(process.pid, 'SIGUSR2');
     });
 });
 // For app termination
-process.on('SIGINT', function() {
-    gracefulShutdown('app termination', function() {
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
         process.exit(0);
     });
 });
 // For Heroku app termination
-process.on('SIGTERM', function() {
-    gracefulShutdown('Heroku app termination', function() {
+process.on('SIGTERM', () => {
+    gracefulShutdown('Heroku app termination', () => {
         process.exit(0);
     });
 });
