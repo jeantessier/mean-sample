eaf97264f54f0508e09b610f7b320edf0c651763
Jean Tessier <jean@jeantessier.com>
2023-09-01 12:11:48 -0700

    Update outdated dependencies
        npm update
    
    The lastest `mongoose` changed its API to use Promises instead of
    callbacks.  I had to update `passport.js` accordingly, using sugestions
    from https://stackoverflow.com/questions/75649330/mongooseerror-model-findone-no-longer-accepts-a-callback-at-function

--- app_api/config/passport.js
+++ app_api/config/passport.js
@@ -7,19 +7,18 @@ passport.use(new LocalStrategy({
         usernameField: 'email'
     },
     (username, password, done) => {
-        User.findOne({ email: username }, (err, user) => {
-            if (err) { return done(err) }
+        User.findOne({
+            email: username
+        }).then(user => {
             if (!user) {
-                return done(null, false, {
-                    message: 'Incorrect username.'
-                })
+                return done(null, false, { message: 'Incorrect username.' })
             }
             if (!user.validPassword(password)) {
-                return done(null, false, {
-                    message: 'Incorrect password.'
-                })
+                return done(null, false, { message: 'Incorrect password.' })
             }
             return done(null, user)
+        }).catch(err => {
+            return done(err)
         })
     }
 ))
